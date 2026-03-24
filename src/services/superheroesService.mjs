11649f647eb3717b386db2f467b76ec1ea7783e5
Mar José <de.la.taes@gmail.com>
2026-03-24 13:24:12 -0300

    agregar cambios

--- src/services/superheroesService.mjs
+++ src/services/superheroesService.mjs
@@ -14,4 +14,4 @@ export async function buscarSuperHeroePorAtributo(atributo, valor) {
 
 export async function obtenerSuperHeroesMayoresDe30() {
     return await SuperHeroRepository.obtenerMayoresDe30();
-},,,
+}
