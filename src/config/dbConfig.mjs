455743a8a0e5cf525972d2544df145337d805faa
Mar José <de.la.taes@gmail.com>
2026-03-24 13:06:14 -0300

    agregar

--- src/config/dbConfig.mjs
+++ src/config/dbConfig.mjs
@@ -4,7 +4,7 @@ export async function connectDB() {
         await mongoose.connect('mongodb+srv://grupo-30:grupo-30@cluster0.blryo.mongodb.net/NodeMod3Cohorte5') ;
     console.log('Conexión exitosa de María José a MongoDB');
     } catch (error) {
-        console.error('Error al conectar a MongoDB María José, intente más tarde:', error);
+        console.error('Error al conectar a MongoDB María José, intenta más tarde:', error);
         Process.exit(1);
     }
 }
